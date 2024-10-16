d269b22f3526fec3950f8e22345348a2548ccd53
neznau123123 <aseevdanil10@yandex.ru>
2024-10-16 10:59:13 +0700

    And function sayName

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -3,5 +3,9 @@ package org.example
 //TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
 // click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
 fun main(){
+    sayName(name = "Artem")
+}
 
+fun sayName(name : String){
+    println("Твое имя это $name")
 }
